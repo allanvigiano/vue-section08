bbd849369b62cf7149df597d8ce28aba4433adc2
Allan Vigiano <allanvigiano@gmail.com>
2017-02-14 18:02:17 -0200

    Class 105

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'name',
+        ],
+        data: function () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
