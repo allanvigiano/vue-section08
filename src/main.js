c87362f03840945636847e3493f0ce0380fe073c
Allan Vigiano <allanvigiano@gmail.com>
2017-02-14 21:01:37 -0200

    Class 113 - Centralizing Code in a Event Bus

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-export const eventBus = new Vue();
+/**
+ *  Good to centralize Bus of aplication, data, tasks,
+ * @type {Vue}
+ */
+export const eventBus = new Vue({
+    methods: {
+        changeAge(age) {
+            this.$emit('ageWasEdited',age);
+        }
+    }
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
 
